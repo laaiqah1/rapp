eab053c71641d131c769517c246ce5fdff99d6ba
laaiqah1 <mat23la.shaikh@pg.ictmumbai.edu.in>
2024-11-29 17:54:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import precision_score, recall_score
-from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import RocCurveDisplay
+
+from sklearn.metrics import plot_precision_recall_curve
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
